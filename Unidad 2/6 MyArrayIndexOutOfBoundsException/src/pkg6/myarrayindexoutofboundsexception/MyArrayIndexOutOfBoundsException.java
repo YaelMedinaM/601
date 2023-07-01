4c0dc85cbf6edd1b506375b3a3d6fa47e4c86d0b
YaelMedinaM <jarkots65@gmail.com>
2023-07-01 17:45:22 -0600

    Se añade ejecicio 6 de ArrayIndexOutOfBoundsException

---
+++ Unidad 2/6 MyArrayIndexOutOfBoundsException/src/pkg6/myarrayindexoutofboundsexception/MyArrayIndexOutOfBoundsException.java
@@ -0,0 +1,15 @@
+package pkg6.myarrayindexoutofboundsexception;
+import javax.swing.JOptionPane;
+
+public class MyArrayIndexOutOfBoundsException extends IndexOutOfBoundsException{
+    public static void main(String[] args) {
+            int[] array= {1,2,3};
+            int x;
+        try {
+           x=array[3];
+        } 
+        catch (ArrayIndexOutOfBoundsException n) {
+            System.out.println(n.getMessage()+" No es registro dentro del Array");
+          }    
+       }
+    }
